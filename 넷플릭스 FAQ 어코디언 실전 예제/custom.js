c065f613e922f9833d48bdc860ebad740768a5b0
shong <shongtruth@naver.com>
2022-07-06 07:37:19 +0900

    20220706

--- 넷플릭스 FAQ 어코디언 실전 예제/custom.js
+++ 넷플릭스 FAQ 어코디언 실전 예제/custom.js
@@ -1,7 +1,7 @@
 $(function(){
     $('.accordion .content').eq(0).show()
     $('.accordion .title').click(function(){
-        $(this).siblings('.accordion .content').slideUp()
+        $(this).siblings('.accordion .content').stop().slideUp()
         $(this).next().stop().slideToggle(300)
         $(this).toggleClass('active')
         $(this).siblings('.accordion .title').removeClass('active')
